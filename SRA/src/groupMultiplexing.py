1f611a15d137284ac443cec50f178f159f483e34
ubut-hnrg <arieljberenstein@gmail.com>
2020-03-20 09:37:32 -0300

    add script to symlink SRA to SRR accessions

--- SRA/src/groupMultiplexing.py
+++ SRA/src/groupMultiplexing.py
@@ -7,18 +7,18 @@ def get_args():
     parser = argparse.ArgumentParser(description='given a list of SRA ids, get its metadata info and download the associated RUN ids')
     parser.add_argument('--inputfile','-f',dest = 'inputfile',help='SRA list with metadata in tsv format')
     parser.add_argument('--outpath','-o',dest = 'outpath',help='root for the directory organization of sampes')
-    parser.add_argument('--runid_path','-r',dest = 'runidpath',help='root for the directory of RUN accession')
+    parser.add_argument('--runidpath','-r',dest = 'runidpath',help='root for the directory of RUN accession')
     
 
     args = parser.parse_args()
 
     ifile = args.inputfile
     opath = args.outpath
-    runidpath = args.runid_path
+    runidpath = args.runidpath
 
     return(ifile,opath,runidpath)
 
-def makesyml(ifile,runidpath,opath):
+def makesyml(ifile,opath,runidpath):
     df = pd.read_csv(ifile,sep = '\t')
     samplenames = df.sample_accession
     mapa = {}
@@ -45,7 +45,7 @@ def makesyml(ifile,runidpath,opath):
 
 def main():
     ifile,opath,runidpath = get_args()
-    makesyml = makedirs(ifile,runidpath,opath)
+    makesyml = makedirs(ifile,opath,runidpath)
 
 if __name__ == '__main__':
     main()
