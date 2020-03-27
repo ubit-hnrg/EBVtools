35e512b36c06e8aa3b8286de92204ea0fe977aac
ubut-hnrg <arieljberenstein@gmail.com>
2020-03-26 23:58:16 -0300

    fixing minor bug

--- pipelineNGS/Map2Refpipeline.sh
+++ pipelineNGS/Map2Refpipeline.sh
@@ -87,7 +87,7 @@ fi
 ################     maping stage      #################
 
 	#Map to reference
-	bwa mem -K 100000000 -v 1 -t 4 $referenceEBV <(zcat $outtrimmed/$s.good.trimmed_1.fastq.gz) <(zcat $outtrimmed/$s.good.trimmed_2.fastq.gz) | samtools view -b - > $outpath/$s/$s.bam
+	bwa mem -K 100000000 -v 1 -t 4 $referenceEBV '<(zcat '$outtrimmed/$s.good.trimmed_1.fastq.gz') <(zcat '$outtrimmed/$s'.good.trimmed_2.fastq.gz)' | samtools view -b - > $outpath/$s/$s.bam
 	
 	#Remove duplicates and unmapped reads
 	samtools view -b -F $FilterBinaryCode $outpath/$s/$s.bam > $outpath/$s/$s.mapped.bam  
